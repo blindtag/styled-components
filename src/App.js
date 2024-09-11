c3c4311f2e27cd59d232455d8df5325013d1822a
unknown <phylace0@gmail.com>
2024-09-11 12:27:09 -0700

    first

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import {} from './components/styled/Container.styled';
+
+function App() {
+  return (
+    <Container >
+      <h1>Hello World</h1>
+    </Container>
+  );
+}
+
+export default App;
